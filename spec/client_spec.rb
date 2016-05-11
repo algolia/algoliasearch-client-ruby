a6f968062d0a111a67c1107902d1cc2fa57cd042
Sylvain UTARD <sylvain.utard@gmail.com>
2016-05-11 09:32:29 +0200

    Improve the ACL tests waiting for the key instead of sleeping

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -373,50 +373,109 @@ describe 'Client' do
     res['facets']['g']['g2'].should be_nil
   end
 
-  xit "should test keys" do
+  def wait_key(index, key, &block)
+    1.upto(60) do # do not wait too long
+      begin
+        k = index.get_user_key(key)
+        if block_given?
+          return if yield k
+          # not found
+          sleep 1
+          next
+        end
+        return
+      rescue
+        # not found
+        sleep 1
+      end
+    end
+  end
+
+  def wait_key_missing(index, key)
+    1.upto(60) do # do not wait too long
+      begin
+        k = index.get_user_key(key)
+        sleep 1
+      rescue
+        # not found
+        return
+      end
+    end
+  end
+
+  def wait_global_key(key, &block)
+    1.upto(60) do # do not wait too long
+      begin
+        k = Algolia.get_user_key(key)
+        if block_given?
+          return if yield k
+          # not found
+          sleep 1
+          next
+        end
+        return
+      rescue
+        # not found
+        sleep 1
+      end
+    end
+  end
+
+  def wait_global_key_missing(key)
+    1.upto(60) do # do not wait too long
+      begin
+        k = Algolia.get_user_key(key)
+        sleep 1
+      rescue
+        # not found
+        return
+      end
+    end
+  end
+
+  it "should test index keys" do
+    @index.set_settings!({}) # ensure the index exists
     resIndex = @index.list_user_keys
     newIndexKey = @index.add_user_key(['search'])
     newIndexKey['key'].should_not eq("")
-    sleep 5 # no task ID here
+    wait_key(@index, newIndexKey['key'])
     resIndexAfter = @index.list_user_keys
     is_include(resIndex['keys'], 'value', newIndexKey['key']).should eq(false)
     is_include(resIndexAfter['keys'], 'value', newIndexKey['key']).should eq(true)
     indexKey = @index.get_user_key(newIndexKey['key'])
     indexKey['acl'][0].should eq('search')
     @index.update_user_key(newIndexKey['key'], ['addObject'])
-    sleep 5 # no task ID here
+    wait_key(@index, newIndexKey['key']) do |key|
+      key['acl'] == ['addObject']
+    end
     indexKey = @index.get_user_key(newIndexKey['key'])
     indexKey['acl'][0].should eq('addObject')
     @index.delete_user_key(newIndexKey['key'])
-    sleep 5 # no task ID here
+    wait_key_missing(@index, newIndexKey['key'])
     resIndexEnd = @index.list_user_keys
     is_include(resIndexEnd['keys'], 'value', newIndexKey['key']).should eq(false)
+  end
 
-
+  it "should test global keys" do
     res = Algolia.list_user_keys
     newKey = Algolia.add_user_key(['search'])
     newKey['key'].should_not eq("")
-    sleep 5 # no task ID here
+    wait_global_key(newKey['key'])
     resAfter = Algolia.list_user_keys
     is_include(res['keys'], 'value', newKey['key']).should eq(false)
     is_include(resAfter['keys'], 'value', newKey['key']).should eq(true)
     key = Algolia.get_user_key(newKey['key'])
     key['acl'][0].should eq('search')
     Algolia.update_user_key(newKey['key'], ['addObject'])
-    sleep 5 # no task ID here
+    wait_global_key(newKey['key']) do |key|
+      key['acl'] == ['addObject']
+    end
     key = Algolia.get_user_key(newKey['key'])
     key['acl'][0].should eq('addObject')
     Algolia.delete_user_key(newKey['key'])
-    sleep 5 # no task ID here
+    wait_global_key_missing(newKey['key'])
     resEnd = Algolia.list_user_keys
     is_include(resEnd['keys'], 'value', newKey['key']).should eq(false)
-  end
-
-  it "should check functions" do
-    @index.get_settings
-    @index.list_user_keys
-    Algolia.list_user_keys
-
   end
 
   it "should handle slash in objectId" do
