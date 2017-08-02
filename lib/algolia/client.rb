df3a95593527e2993839d0214cb510b27d092f88
Matthieu Dumont <jerska@live.fr>
2017-08-02 15:45:21 +0200

    chore(request_options): group {create,update}_api_key params

--- lib/algolia/client.rb
+++ lib/algolia/client.rb
@@ -248,9 +248,18 @@ module Algolia
     #
     #  Create a new user key
     #
-    #  @param obj can be two different parameters:
-    #        The list of parameters for this key. Defined by a NSDictionary that
-    #        can contains the following values:
+    #  Deprecated call was add_api_key(acl, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
+    #
+    #  ACL can contain an array with those strings:
+    #    - search: allow to search (https and http)
+    #    - addObject: allows to add/update an object in the index (https only)
+    #    - deleteObject : allows to delete an existing object (https only)
+    #    - deleteIndex : allows to delete index content (https only)
+    #    - settings : allows to get index settings (https only)
+    #    - editSettings : allows to change index settings (https only)
+    #
+    #  @param obj The list of parameters for this key.
+    #         Defined by a Hash that can contain the following values:
     #          - acl: array of string
     #          - indices: array of string
     #          - validity: int
@@ -259,21 +268,9 @@ module Algolia
     #          - maxHitsPerQuery: integer
     #          - queryParameters: string
     #          - maxQueriesPerIPPerHour: integer
-    #        Or the list of ACL for this key. Defined by an array of NSString that
-    #        can contains the following values:
-    #          - search: allow to search (https and http)
-    #          - addObject: allows to add/update an object in the index (https only)
-    #          - deleteObject : allows to delete an existing object (https only)
-    #          - deleteIndex : allows to delete index content (https only)
-    #          - settings : allows to get index settings (https only)
-    #          - editSettings : allows to change index settings (https only)
-    #  @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-    #  @param maxQueriesPerIPPerHour the maximum number of API calls allowed from an IP address per hour (0 means unlimited)
-    #  @param maxHitsPerQuery  the maximum number of hits this API key can retrieve in one call (0 means unlimited)
-    #  @param indexes the optional list of targeted indexes
-    #  @param request_options contains extra parameters to send with your query
+    #  @param request_options contains extra parameters to send with your query - Default = {}
     #
-    def add_api_key(obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
+    def add_api_key(obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
       if obj.instance_of? Array
         params = {
           :acl => obj
@@ -281,14 +278,17 @@ module Algolia
       else
         params = obj
       end
+
+      validity = request_options unless request_options.is_a?(Hash)
+
       if validity != 0
-        params["validity"] = validity.to_i
+        params['validity'] = validity.to_i
       end
       if maxQueriesPerIPPerHour != 0
-        params["maxQueriesPerIPPerHour"] = maxQueriesPerIPPerHour.to_i
+        params['maxQueriesPerIPPerHour'] = maxQueriesPerIPPerHour.to_i
       end
       if maxHitsPerQuery != 0
-        params["maxHitsPerQuery"] = maxHitsPerQuery.to_i
+        params['maxHitsPerQuery'] = maxHitsPerQuery.to_i
       end
       params[:indexes] = indexes if indexes
       post(Protocol.keys_uri, params.to_json, :write, request_options)
@@ -297,9 +297,19 @@ module Algolia
     #
     #  Update a user key
     #
-    #  @param obj can be two different parameters:
-    #        The list of parameters for this key. Defined by a NSDictionary that
-    #        can contains the following values:
+    #  Deprecated call was update_api_key(key, acl, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
+    #
+    #  ACL can contain an array with those strings:
+    #    - search: allow to search (https and http)
+    #    - addObject: allows to add/update an object in the index (https only)
+    #    - deleteObject : allows to delete an existing object (https only)
+    #    - deleteIndex : allows to delete index content (https only)
+    #    - settings : allows to get index settings (https only)
+    #    - editSettings : allows to change index settings (https only)
+    #
+    #  @param key API Key to update
+    #  @param obj The list of parameters for this key.
+    #         Defined by a Hash that can contain the following values:
     #          - acl: array of string
     #          - indices: array of string
     #          - validity: int
@@ -308,21 +318,9 @@ module Algolia
     #          - maxHitsPerQuery: integer
     #          - queryParameters: string
     #          - maxQueriesPerIPPerHour: integer
-    #        Or the list of ACL for this key. Defined by an array of NSString that
-    #        can contains the following values:
-    #          - search: allow to search (https and http)
-    #          - addObject: allows to add/update an object in the index (https only)
-    #          - deleteObject : allows to delete an existing object (https only)
-    #          - deleteIndex : allows to delete index content (https only)
-    #          - settings : allows to get index settings (https only)
-    #          - editSettings : allows to change index settings (https only)
-    #  @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-    #  @param maxQueriesPerIPPerHour the maximum number of API calls allowed from an IP address per hour (0 means unlimited)
-    #  @param maxHitsPerQuery  the maximum number of hits this API key can retrieve in one call (0 means unlimited)
-    #  @param indexes the optional list of targeted indexes
-    #  @param request_options contains extra parameters to send with your query
+    #  @param request_options contains extra parameters to send with your query - Default = {}
     #
-    def update_api_key(key, obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
+    def update_api_key(key, obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
       if obj.instance_of? Array
         params = {
           :acl => obj
@@ -330,14 +328,17 @@ module Algolia
       else
         params = obj
       end
+
+      validity = request_options unless request_options.is_a?(Hash)
+
       if validity != 0
-        params["validity"] = validity.to_i
+        params['validity'] = validity.to_i
       end
       if maxQueriesPerIPPerHour != 0
-        params["maxQueriesPerIPPerHour"] = maxQueriesPerIPPerHour.to_i
+        params['maxQueriesPerIPPerHour'] = maxQueriesPerIPPerHour.to_i
       end
       if maxHitsPerQuery != 0
-        params["maxHitsPerQuery"] = maxHitsPerQuery.to_i
+        params['maxHitsPerQuery'] = maxHitsPerQuery.to_i
       end
       params[:indexes] = indexes if indexes
       put(Protocol.key_uri(key), params.to_json, :write, request_options)
@@ -672,6 +673,16 @@ module Algolia
   #
   #  Create a new user key
   #
+  #  Deprecated call was add_api_key(acl, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
+  #
+  #  ACL can contain an array with those strings:
+  #    - search: allow to search (https and http)
+  #    - addObject: allows to add/update an object in the index (https only)
+  #    - deleteObject : allows to delete an existing object (https only)
+  #    - deleteIndex : allows to delete index content (https only)
+  #    - settings : allows to get index settings (https only)
+  #    - editSettings : allows to change index settings (https only)
+  #
   #  @param obj can be two different parameters:
   #        The list of parameters for this key. Defined by a NSDictionary that
   #        can contains the following values:
@@ -683,35 +694,33 @@ module Algolia
   #          - maxHitsPerQuery: integer
   #          - queryParameters: string
   #          - maxQueriesPerIPPerHour: integer
-  #        Or the list of ACL for this key. Defined by an array of NSString that
-  #        can contains the following values:
-  #          - search: allow to search (https and http)
-  #          - addObject: allows to add/update an object in the index (https only)
-  #          - deleteObject : allows to delete an existing object (https only)
-  #          - deleteIndex : allows to delete index content (https only)
-  #          - settings : allows to get index settings (https only)
-  #          - editSettings : allows to change index settings (https only)
-  #  @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-  #  @param maxQueriesPerIPPerHour the maximum number of API calls allowed from an IP address per hour (0 means unlimited)
-  #  @param maxHitsPerQuery  the maximum number of hits this API key can retrieve in one call (0 means unlimited)
-  #  @param indexes the optional list of targeted indexes
-  #  @param request_options contains extra parameters to send with your query
+  #  @param request_options contains extra parameters to send with your query - Default = {}
   #
-  def Algolia.add_api_key(obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
-    Algolia.client.add_api_key(obj, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes, request_options)
+  def Algolia.add_api_key(obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
+    Algolia.client.add_api_key(obj, request_options, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
   end
 
   # Deprecated
-  def Algolia.add_user_key(obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
-    Algolia.client.add_api_key(obj, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes, request_options)
+  def Algolia.add_user_key(obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
+    Algolia.client.add_api_key(obj, request_options, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
   end
 
   #
   #  Update a user key
   #
-  #  @param obj can be two different parameters:
-  #        The list of parameters for this key. Defined by a NSDictionary that
-  #        can contains the following values:
+  #  Deprecated call was update_api_key(key, acl, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
+  #
+  #  ACL can contain an array with those strings:
+  #    - search: allow to search (https and http)
+  #    - addObject: allows to add/update an object in the index (https only)
+  #    - deleteObject : allows to delete an existing object (https only)
+  #    - deleteIndex : allows to delete index content (https only)
+  #    - settings : allows to get index settings (https only)
+  #    - editSettings : allows to change index settings (https only)
+  #
+  #  @param key API Key to update
+  #  @param obj The list of parameters for this key.
+  #         Defined by a Hash that can contain the following values:
   #          - acl: array of string
   #          - indices: array of string
   #          - validity: int
@@ -720,27 +729,15 @@ module Algolia
   #          - maxHitsPerQuery: integer
   #          - queryParameters: string
   #          - maxQueriesPerIPPerHour: integer
-  #        Or the list of ACL for this key. Defined by an array of NSString that
-  #        can contains the following values:
-  #          - search: allow to search (https and http)
-  #          - addObject: allows to add/update an object in the index (https only)
-  #          - deleteObject : allows to delete an existing object (https only)
-  #          - deleteIndex : allows to delete index content (https only)
-  #          - settings : allows to get index settings (https only)
-  #          - editSettings : allows to change index settings (https only)
-  #  @param validity the number of seconds after which the key will be automatically removed (0 means no time limit for this key)
-  #  @param maxQueriesPerIPPerHour the maximum number of API calls allowed from an IP address per hour (0 means unlimited)
-  #  @param maxHitsPerQuery  the maximum number of hits this API key can retrieve in one call (0 means unlimited)
-  #  @param indexes the optional list of targeted indexes
-  #  @param request_options contains extra parameters to send with your query
+  #  @param request_options contains extra parameters to send with your query - Default = {}
   #
-  def Algolia.update_api_key(key, obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
-    Algolia.client.update_api_key(key, obj, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes, request_options)
+  def Algolia.update_api_key(key, obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
+    Algolia.client.update_api_key(key, obj, request_options, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
   end
 
   # Deprecated
-  def Algolia.update_user_key(key, obj, validity = 0, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil, request_options = {})
-    Algolia.client.update_api_key(key, obj, validity, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes, request_options)
+  def Algolia.update_user_key(key, obj, request_options = nil, maxQueriesPerIPPerHour = 0, maxHitsPerQuery = 0, indexes = nil)
+    Algolia.client.update_api_key(key, obj, request_options, maxQueriesPerIPPerHour, maxHitsPerQuery, indexes)
   end
 
   # Delete an existing user key
