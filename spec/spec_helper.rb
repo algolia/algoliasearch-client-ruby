bcaf5e44ab83a66c3735051ac01b9a0af37029ac
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2017-10-12 12:53:34 -0300

    Allow to reuse the algolia webmocks
    This is needed because when you require algolia/webmock, the mocks will
    work only in one spec at rspec, if you required webmock/rspec.
    
    This happens because webmock/rspec will clear all stubs between examples
    and we need to stub again for the next one.
    
    The new approach is:
    
        require 'algolia/webmock'
    
        RSpec.configure do |config|
          config.before(:each) { Algolia::WebMock.mock! }
        end

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,8 @@ require 'rspec'
 require 'webmock/rspec'
 WebMock.disable!
 
+require 'algolia/webmock'
+
 raise "missing ALGOLIA_APPLICATION_ID or ALGOLIA_API_KEY environment variables" if ENV['ALGOLIA_APPLICATION_ID'].nil? || ENV['ALGOLIA_API_KEY'].nil?
 Algolia.init :application_id => ENV['ALGOLIA_APPLICATION_ID'], :api_key => ENV['ALGOLIA_API_KEY']
 
