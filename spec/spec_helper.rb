896bac347b1b1c1d88d23e3ce07c585938c188f5
raphi <raphi@users.noreply.github.com>
2018-03-13 15:08:49 +0100

    Update WebMock and Travis (#268)
    * Add Ruby 2.5.0 to Travis
    
    * Fix WebMock default config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 
 if ENV['COVERAGE']
-    require 'simplecov'
-    SimpleCov.start
+  require 'simplecov'
+  SimpleCov.start
 end
 
-if ENV['TRAVIS'] && Object.const_defined?(:RUBY_ENGINE) && RUBY_ENGINE == "ruby"
+if ENV['TRAVIS'] && Object.const_defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'ruby'
   require 'coveralls'
   Coveralls.wear!
 end
@@ -18,15 +18,20 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'algoliasearch'
 require 'rspec'
-
 require 'webmock/rspec'
-WebMock.disable!
-
 require 'algolia/webmock'
 
-raise "missing ALGOLIA_APPLICATION_ID or ALGOLIA_API_KEY environment variables" if ENV['ALGOLIA_APPLICATION_ID'].nil? || ENV['ALGOLIA_API_KEY'].nil?
+raise 'missing ALGOLIA_APPLICATION_ID or ALGOLIA_API_KEY environment variables' if ENV['ALGOLIA_APPLICATION_ID'].nil? || ENV['ALGOLIA_API_KEY'].nil?
 Algolia.init :application_id => ENV['ALGOLIA_APPLICATION_ID'], :api_key => ENV['ALGOLIA_API_KEY']
 
-RSpec.configure do |c|
-  c.mock_with :rspec
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.before(:suite) do
+    WebMock.disable!
+  end
+
+  config.after(:suite) do
+    WebMock.disable!
+  end
 end
