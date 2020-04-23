f7e0a37e4ca16895f000ccf91d2a16088ed6aaf6
Chloe Liban <chloe.liban@gmail.com>
2020-04-23 12:24:00 +0200

    use of class << self for helpers

--- lib/algolia/helpers.rb
+++ lib/algolia/helpers.rb
@@ -1,33 +1,35 @@
 module Helpers
-  # Convert an Hash to json
-  #
-  def self.to_json(body)
-    body.is_a?(String) ? body : MultiJson.dump(body)
-  end
+  class << self
+    # Convert an Hash to json
+    #
+    def to_json(body)
+      body.is_a?(String) ? body : MultiJson.dump(body)
+    end
 
-  def self.to_string(opt)
-    opt.is_a?(String) ? opt : opt.to_s
-  end
+    def to_string(opt)
+      opt.is_a?(String) ? opt : opt.to_s
+    end
 
-  # Convert params to a full query string
-  #
-  def self.handle_params(params)
-    params.nil? || params.empty? ? '' : "?#{to_query_string(params)}"
-  end
+    # Convert params to a full query string
+    #
+    def handle_params(params)
+      params.nil? || params.empty? ? '' : "?#{to_query_string(params)}"
+    end
 
-  # Create a query string from params
-  #
-  def self.to_query_string(params)
-    params.map do |key, value|
-      "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
-    end.join('&')
-  end
+    # Create a query string from params
+    #
+    def to_query_string(params)
+      params.map do |key, value|
+        "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
+      end.join('&')
+    end
 
-  def self.json_to_hash(json)
-    MultiJson.load(json)
-  end
+    def json_to_hash(json)
+      MultiJson.load(json)
+    end
 
-  def self.get_option(hash, key)
-    hash[key.to_sym] || {}
+    def get_option(hash, key)
+      hash[key.to_sym] || {}
+    end
   end
 end
