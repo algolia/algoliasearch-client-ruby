d4c9261a0af49cb3dc1c8d75e4708181f00e2f12
Chloe Liban <chloe.liban@gmail.com>
2020-04-27 18:15:18 +0200

    small fix on error catching

--- test/algolia/integration/search_index_test.rb
+++ test/algolia/integration/search_index_test.rb
@@ -138,7 +138,7 @@ class SearchIndexTest < BaseTest
       assert_equal 2, response['nbHits']
       assert_equal 0, Algolia::Search::Index.get_object_position(response, 'nicolas-dessaigne')
       assert_equal 1, Algolia::Search::Index.get_object_position(response, 'julien-lemoine')
-      assert_equal -1, Algolia::Search::Index.get_object_position(response, '')
+      assert_equal (-1), Algolia::Search::Index.get_object_position(response, '')
     end
 
     def find_objects
