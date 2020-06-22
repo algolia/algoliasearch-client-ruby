dcb75e191abe3d4367bf2bf2908e4075b2168e86
Chloe Liban <chloe.liban@gmail.com>
2020-06-22 13:54:02 +0200

    fix: move request options file

--- test/algolia/integration/search_index_test.rb
+++ test/algolia/integration/search_index_test.rb
@@ -2,37 +2,23 @@ require 'httpclient'
 require_relative 'base_test'
 
 class SearchIndexTest < BaseTest
-  describe 'customize search client' do
-    def test_with_custom_adapter
-      client = Algolia::Search::Client.new(@@search_config, adapter: 'httpclient')
-      index  = client.init_index(get_test_index_name('test_custom_adapter'))
-
-      index.save_object!({name: 'test', data: 10}, {auto_generate_object_id_if_not_exist: true})
-      response = index.search('test')
-
-      refute_empty response[:hits]
-      assert_equal 'test', response[:hits][0][:name]
-      assert_equal 10, response[:hits][0][:data]
+  describe 'pass request options' do
+    def before_all
+      super
+      @index = @@search_client.init_index(get_test_index_name('options'))
     end
 
-    def test_with_custom_requester
-      client = Algolia::Search::Client.new(@@search_config, http_requester: MockRequester.new)
-      index  = client.init_index(get_test_index_name('test_custom_requester'))
+    def test_with_wrong_credentials
+      exception = assert_raises Algolia::AlgoliaHttpError do
+        @index.save_object(generate_object('111'), {
+          headers: {
+            'X-Algolia-Application-Id' => 'XXXXX',
+            'X-Algolia-API-Key' => 'XXXXX'
+          }
+        })
+      end
 
-      response = index.search('test')
-
-      refute_nil response[:hits]
-    end
-
-    def test_without_providing_config
-      client   = Algolia::Search::Client.create(APPLICATION_ID_1, ADMIN_KEY_1)
-      index    = client.init_index(get_test_index_name('test_no_config'))
-      index.save_object!({name: 'test', data: 10}, {auto_generate_object_id_if_not_exist: true})
-      response = index.search('test')
-
-      refute_empty response[:hits]
-      assert_equal 'test', response[:hits][0][:name]
-      assert_equal 10, response[:hits][0][:data]
+      assert_equal 'Invalid Application-ID or API key', exception.message
     end
   end
 
