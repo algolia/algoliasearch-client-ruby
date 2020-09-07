2fee3aa172ffdd597deb0627fde0c0386e25caca
Chloe Liban <chloe.liban@gmail.com>
2020-09-07 12:22:23 +0200

    feat(steep): adds responses

--- lib/algolia/responses/update_api_key_response.rb
+++ lib/algolia/responses/update_api_key_response.rb
@@ -1,5 +1,6 @@
 module Algolia
   class UpdateApiKeyResponse < BaseResponse
+    # @dynamic raw_response
     attr_reader :raw_response
 
     # @param client [Search::Client] Algolia Search Client used for verification
