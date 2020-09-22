584536b87c854190c085311b2b2306c2afab29e4
Chloe Liban <chloe.liban@gmail.com>
2020-09-22 16:54:51 +0200

    fix(timeouts): revert read and write timeouts (#417)

--- lib/algolia/defaults.rb
+++ lib/algolia/defaults.rb
@@ -24,8 +24,8 @@ module Defaults
 
   BATCH_SIZE      = 1000
   CONNECT_TIMEOUT = 2
-  READ_TIMEOUT    = 30
-  WRITE_TIMEOUT   = 5
+  READ_TIMEOUT    = 5
+  WRITE_TIMEOUT   = 30
   USER_AGENT      = "Algolia for Ruby (#{Algolia::VERSION}), Ruby (#{RUBY_VERSION})"
 
   WAIT_TASK_DEFAULT_TIME_BEFORE_RETRY = 100
