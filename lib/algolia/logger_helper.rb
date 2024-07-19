fbebb48d73c0c58ffb06be162bee973ac0cdc5a3
Thomas Dalous <thomas.dalous@algolia.com>
2024-07-19 11:31:08 +0200

    fix(debug): make sure it always fallback to stderr

--- lib/algolia/logger_helper.rb
+++ lib/algolia/logger_helper.rb
@@ -5,8 +5,17 @@ module Algolia
     # @param debug_file [nil|String] file used to output the logs
     #
     def self.create(debug_file = nil)
-      file              = debug_file || (ENV['ALGOLIA_DEBUG'] ? File.open('debug.log', 'a+') : $stderr)
-      instance          = ::Logger.new file
+      file = debug_file
+
+      if file.nil? && ENV['ALGOLIA_DEBUG']
+        begin
+          file = File.new('debug.log', 'a+')
+        rescue Errno::EACCES, Errno::ENOENT => e
+          puts "Failed to open debug.log: #{e.message}. Falling back to $stderr."
+        end
+      end
+
+      instance = ::Logger.new(file || $stderr)
       instance.progname = 'algolia'
       instance
     end
